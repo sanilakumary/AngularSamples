f619c91bbd9332c95de52e269bf2b15683d77d51
sanilakumary <48491134+sanilakumary@users.noreply.github.com>
2019-08-14 16:41:51 +0200

    Add files via upload

---
+++ product.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Product} from './product';
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ProductService
+{
+  url="https://localhost:44371/api/v1/Product";
+  constructor(private http:HttpClient){}
+  getAllProdcuts():Observable<Product[]>
+  {
+    return this.http.get<Product[]>
+    //(this.url + '/GetAllAProdcucts');
+    (this.url);
+  }
+  getProductById(id:string):Observable<Product>
+  {
+    //return this.http.get<Product>(this.url + '/GetEmployeeById/'+id);
+    return this.http.get<Product>(this.url + '/'+id);
+  }
+  CreateProduct(product:Product):Observable<Product>
+  {
+    const httpOptions = {headers:new HttpHeaders({'ContentType' : 'application/json'})};
+    //return this.http.post<Product>(this.url + '/CreateProduct/',product,httpOptions);
+    return this.http.post<Product>(this.url,product,httpOptions);
+  }
+  UpdateProduct(product:Product):Observable<Product>
+  {
+    const httpOptions = {headers:new HttpHeaders({'ContentType':'application/json'})};
+    //return this.http.put<Product>(this.url + '/UpdateProduct/',product,httpOptions);
+    return this.http.put<Product>(this.url ,product,httpOptions);
+  }
+  DeleteProduct(id:string):Observable<number>
+  {
+    const httpOptions = {headers: new HttpHeaders({'ContentType':'application/json'})};
+    //return this.http.delete<number>(this.url + '/DeleteProduct?id=' + id,httpOptions);
+    return this.http.delete<number>(this.url + '?id=' + id,httpOptions);
+  }
+}
